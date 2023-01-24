889d400f49d4fa9f167d520122339365755ed342
Rodrigues06SAntos <victorrodrigue06@gmail.com>
2023-01-24 10:30:42 -0300

    templatess

--- app/routes.py
+++ app/routes.py
@@ -6,19 +6,19 @@ from app.forms import SendForm
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', titulo = 'home')
 
 @app.route('/contato', methods = ['POST', 'GET'])
 def contato():
     form = SendForm()
     if form.validate_on_submit():
         mensagem = flash('A mensagem foi enviada com sucesso')
-    return render_template('contato.html', form=form)
+    return render_template('contato.html', form=form, titulo = 'contato')
 
 @app.route('/blog')
 def blog():
-    return render_template('blog.html')
+    return render_template('blog.html', titulo = 'blog')
 
 @app.route('/sobre')
 def sobre():
-    return render_template('sobre.html')
+    return render_template('sobre.html', titulo = 'sobre')
